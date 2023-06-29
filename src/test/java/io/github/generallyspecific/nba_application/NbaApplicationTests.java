194249b2e753d3d49429444c5f74811bc8afb072
David Lin <dlin88@binghamton.edu>
2023-06-29 01:09:42 -0400

    Set up bare minimum

---
+++ src/test/java/io/github/generallyspecific/nba_application/NbaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.generallyspecific.nba_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
